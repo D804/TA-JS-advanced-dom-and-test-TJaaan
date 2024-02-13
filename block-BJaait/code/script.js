87eeb801aeda616ec5e751934447686b955d5766
D804 <kushwahadeepakchange121@gmail.com>
2024-02-13 13:56:10 +0530

    Drag and drop Assignment

---
+++ block-BJaait/code/script.js
@@ -0,0 +1,10 @@
+let click = document.querySelector("icon");
+let innerInput = document.querySelector("input");
+let ul = document.createElement("ul");
+let li = document.createElement("li");
+li.classList.add(draggable = "true");
+ul.append("li"); 
+click.addEventListener("click",function(e){
+e.innerHTML = li.innerHTML;
+
+});
